167ce40bad22b167b15e80630745975f4d86192e
Jakub Wilk <jwilk@jwilk.net>
2019-01-12 18:30:27 +0100

    locale: detect language and writing system.
    We only need to distinguish between Latin and non-Latin.

--- locale.c
+++ locale.c
@@ -2,22 +2,53 @@
  * SPDX-License-Identifier: MIT
  */
 
+#define _POSIX_C_SOURCE 200809L
+
 #include <langinfo.h>
 #include <locale.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "locale.h"
 
 char* dochttx_charset = NULL;
+char* dochttx_lang = NULL;
+bool dochttx_latin = true;
 
 int dochttx_locale_init(void)
 {
-  if (setlocale(LC_ALL, "") == NULL)
+  const char *locale;
+  locale = setlocale(LC_ALL, "");
+  if (locale == NULL)
     return -1;
   dochttx_charset = strdup(nl_langinfo(CODESET));
   if (dochttx_charset == NULL)
     return -1;
+  locale = setlocale(LC_CTYPE, "");
+  if (locale == NULL)
+    return -1;
+  const char *tmp = strchr(locale, '_');
+  if (tmp == NULL)
+    dochttx_lang = strdup(locale);
+  else
+    dochttx_lang = strndup(locale, tmp - locale);
+  if (dochttx_lang == NULL)
+    return -1;
+  locale = setlocale(LC_TIME, locale);
+  if (locale == NULL)
+    return -1;
+  const char *text = nl_langinfo(MON_9);
+  dochttx_latin = false;
+  for (tmp=text; *tmp; tmp++)
+    if ((*tmp >= 'a' && *tmp <= 'z') || (*tmp >= 'A' && *tmp <= 'Z')) {
+      dochttx_latin = true;
+      break;
+    }
+  locale = setlocale(LC_TIME, "");
+  if (locale == NULL)
+    return -1;
+  abort();
   return 0;
 }
 
--- locale.h
+++ locale.h
@@ -1,7 +1,11 @@
 #ifndef DOCHTTX_LOCALE_H
 #define DOCHTTX_LOCALE_H
 
+#include <stdbool.h>
+
 extern char* dochttx_charset;
+extern char* dochttx_lang;
+extern bool dochttx_latin;
 
 int dochttx_locale_init(void);
 void dochttx_locale_quit(void);
